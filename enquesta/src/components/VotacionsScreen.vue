0d63f08973d8e643960ef6bf0991816546f68ce4
Deraybcbc <121021917+Deraybcbc@users.noreply.github.com>
2025-05-24 21:42:15 +0200

    cositas

--- enquesta/src/components/VotacionsScreen.vue
+++ enquesta/src/components/VotacionsScreen.vue
@@ -6,10 +6,10 @@ const votacion = useVotacionsScreeen();
 
 <template>
   <q-page-container>
-    <div class="q-pa-md flex flex-center full-height">
+    <div class="q-pa-md flex flex-center wrap full-height">
       <h2>Bienvenido {{ votacion.loginInfo.username }}</h2>
 
-      <div v-for="(votos, index) in votacion.votosStore.infoVotos" class="q-pa-md q-gutter-sm">
+      <div v-for="(votos, index) in votacion.votosStore.infoVotos" class="q-pa-md q-gutter-sm flex flex-center q-mt-md q-mb-md">
         <p>{{ votos }}</p>
         <q-btn class="glossy" @click="votacion.votar(index)" round color="deep-orange" icon="local_activity"/>
       </div>
